81c3f03158c0b6a670b7d0f6a29ab4313e68dac0
manaschubby <manaschubby@gmail.com>
2023-05-11 02:30:41 +0530

    Errors fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 import Template from "./template";
 import MainPanel from "../components/MainPanel";
 import Officers from "../components/Officers";
